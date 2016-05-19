970631f8a2056da915e286d702348af4885feef7
Bruce Lefebvre <blefebvr@adobe.com>
2016-05-19 10:26:18 +0200

    Added your-app test sample

--- tests/browsers/iphone-6s-9.3.js
+++ tests/browsers/iphone-6s-9.3.js
@@ -5,7 +5,7 @@ module.exports = [
 		'app': currentDir + '/../../platforms/ios/build/emulator/PerfSample.app',
 		'bundleId': 'com.brucelefebvre.PGAppPerf',
 		'platformName': 'iOS',
-		'platformVersion': '9.2',
+		'platformVersion': '9.3',
 		'deviceName': 'iPhone 6s',
 		'autoWebview': true
 	}
